2da1368ae194af8d2f49fe416670e7e53960fe81
blessNcode <blessingoyor2008@gmail.com>
2025-06-16 14:37:19 -0400

    concatenation

--- concatenation.js
+++ concatenation.js
@@ -9,11 +9,21 @@ submitButton.addEventListener("click", function() {
     let birthYear = document.querySelector(".year").value;
     let age = 2023 - birthYear; // update to 2024, if necessary
     // 1. Declare a variable to save the value of the city the user is from.
+    let city = document.querySelector('.city').value;
 
     // 2. Below write your sentences! 
     // - Have them show up in the div with class "results".
     // - Example paragraph: "My name is (myName). I'm feeling (mood) and I absolutely lovveeee eating (food) 😍..."
     // - Try placing your sentences into <p> or <li> tags.
+     let connieResult = "My name is " + myName +
+        ". I'm feeling " + mood +
+        " and I absolutely lovveeee eating " + food +
+        ". I was born in " + birthYear +
+        ", so I am " + age +
+        ". I live in " + city;
 
+
+
+    results.innerHTML = connieResult;
 
 });
